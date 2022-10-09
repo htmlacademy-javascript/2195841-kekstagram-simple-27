69cc7974900a1da15bdc8cf05476894d4b2631ab
polinahudyakova <polinahudyakova3108@gmail.com>
2022-10-09 22:36:34 +0300

    Правка замечаний линтера

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-let getRandomNumber = function(from, to) {
-    return Math.floor(Math.random() * (to - from + 1)) + from;
-}
-    
-let isValidLength = function(value, maxLength) {
-    if (!value) {
-        return false;
-    }
-    return value.length <= maxLength;
-}
+const getRandomNumber = function(from, to) {
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+};
+
+const isValidLength = function(value, maxLength) {
+  if (!value) {
+    return false;
+  }
+  return value.length <= maxLength;
+};
+
+getRandomNumber(2, 5);
+isValidLength('5', 5);
